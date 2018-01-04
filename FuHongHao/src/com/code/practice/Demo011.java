aacecee5f71bab00ae14cc7225e1cb974638de43
Blankmargin <979585525@qq.com>
2018-01-04 23:42:29 +0800

    2018.1.4 homework

---
+++ FuHongHao/src/com/code/practice/Demo011.java
@@ -0,0 +1,14 @@
+package com.code.practice;
+
+public class Demo011 {
+    /**
+     *输出错误信息与调试信息
+     * @param args
+     */
+    public static void main(String[] args) {
+        System.out.println("main()方法开始运行");
+        System.err.println("这是一个错误信息");
+        System.out.println("PrintErrorAndDebug.main()");
+        System.out.println("main()方法运行结束");
+    }
+}
